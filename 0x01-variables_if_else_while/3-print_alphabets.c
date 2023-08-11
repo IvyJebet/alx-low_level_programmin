36fe83553f95daae3ee8aa607194fdd6a0369ced
IvyJebet <jebetivy388@gmail.com>
2023-08-11 07:53:56 -0700

    alphABET Task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+ * main - Prints alphabet in lowercase then in uppercase
+ *
+ * Return= Always (0) Successs
+ */
+int main(void)
+{
+	char lowercase, uppercase;
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++) 
+	{
+		putchar(lowercase);
+	}
+	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++) 
+	{
+		putchar(uppercase);
+	}
+	putchar('\n');
+	return (0);
+}
+
