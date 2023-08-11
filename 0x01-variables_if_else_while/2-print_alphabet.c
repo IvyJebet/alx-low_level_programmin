02cec84c248bf8e27576d4fc2bd338dbab3c648a
IvyJebet <jebetivy388@gmail.com>
2023-08-11 02:41:27 -0700

    C - Variables, if, else, while Task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
- * main - Print the letters.
+ * main - Prints the alphabet.
  *
  * Return: Always 0 (Success)
- * */
-int main() 
+ */
+int main(void) 
 {
-	char letter;
-}
+	char alp[26]= "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-	for (letter = 'a'; letter <= 'z'; letter++) {
-		putchar(letter);
+	for (i = 0; i < 26; i++) {
+		putchar(alp[i]);
 	}
 	putchar("\n");
 	return 0;
