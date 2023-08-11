a4f71b95c3db81d95879eed6ef86b2263d1adcbd
IvyJebet <jebetivy388@gmail.com>
2023-08-11 09:43:48 -0700

    Numberz Task

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,14 +3,15 @@
  * main -  prints all single digit numbers of base 10 starting from 0
  *
  * Return: Always 0 (Success)
- */ 
+ */
 int main(void)
 {
 	int i;
-	for (i = 48; i < 58; i++) 
+	
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
 	putchar('\n');
 	return (0);
-}		
+}
