cf8541a2fd021637985b27365f6149111e4d8ee8
IvyJebet <jebetivy388@gmail.com>
2023-08-17 11:34:39 -0700

    Collaboration is multiplication Task

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * mul - checks for checks (0 through 9)
- * @a: a - variable
+ * mul - checks for checks (0 through 9).
+ * @a: a - Variable
  * @b: b - variable
- * Return: Always 0
+ * Return: Always 0.
  */
 int mul(int a, int b)
 {
