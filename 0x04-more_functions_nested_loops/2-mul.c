3b4b44ef886c4af56328b5dfd6435d0920e896e2
IvyJebet <jebetivy388@gmail.com>
2023-08-17 09:13:54 -0700

    Collaboration is multiplication Task

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,7 +2,7 @@
 /**
  * mul - checks for a digit (0 through 9)
  *@a: a - Variable
-* @b: b - Variable
+* @b: b - variable
  * Return: Always 0
  */
 int mul(int a, int b)
@@ -10,6 +10,5 @@ int mul(int a, int b)
 	int mul;
 
 	mul = a*b;
-
 	return (mul);
 }
