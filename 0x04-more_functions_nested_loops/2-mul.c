1fa6f14f133d98202174ddb75ff9d77123b8acc8
IvyJebet <jebetivy388@gmail.com>
2023-08-17 09:11:19 -0700

    Collaboration is multiplication Task

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * mul - Multiplies two integers
- * ends in a new line
+ * mul - checks for a digit (0 through 9)
  *@a: a - Variable
- @b: b - Variable
+* @b: b - Variable
  * Return: Always 0
  */
 int mul(int a, int b)
