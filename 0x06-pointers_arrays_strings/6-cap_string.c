944f8864555659e9a127a7fb7c8bee71f5fc9468
IvyJebet <jebetivy388@gmail.com>
2023-08-24 04:28:44 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes Task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,8 +13,7 @@ char *cap_string(char *str)
         {       
                 while (!(str[index] >= 'a' && str[index] <= 'z'))
                         index++;
-                
-                if (str[index - 1] == ' ' ||
+		if (str[index - 1] == ' ' ||
                     str[index - 1] == '\t' ||
                     str[index - 1] == '\n' ||
                     str[index - 1] == ',' ||
@@ -29,8 +28,7 @@ char *cap_string(char *str)
 		    str[index - 1] == '}' ||
 		    index == 0)
 			str[index] -= 32;
-
 		index++;
 	}
-	return (str);
+	return (str;
 }
