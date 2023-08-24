316519ae427b1c0bd234d2b6a538850a727242fd
IvyJebet <jebetivy388@gmail.com>
2023-08-24 01:59:38 -0700

    strncat Task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * _strncat - Concatenates two strings using n bytes from src at most
+ * _strncat - Concatenates two strings
+ * using n bytes from src at most
  * @dest: Input value
  * @src: Input value
  * @n: Input value
@@ -12,7 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j;
 
-	i - 0;
+	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
