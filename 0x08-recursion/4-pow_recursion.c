8b3158d866b4c73307cac3ea5e26ec34be8851cc
IvyJebet <jebetivy388@gmail.com>
2023-08-30 09:12:25 -0700

    Once an idea has taken hold of the brain it's almost impossible to eradicate Task

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- *  _pow_recursion - Function that returns the value of x raised to the power of y
+ *  _pow_recursion - Function that returns the value 
+ *  of x raised to the power of y
  *  @x: Value to raise
  *  @y: Power
  *
