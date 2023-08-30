286b61c8fa97b2c1718c0624740d35a84e50f1e9
IvyJebet <jebetivy388@gmail.com>
2023-08-30 11:19:00 -0700

    Inception. Now, before you bother telling me it's impossible... Task

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,7 +16,6 @@ int wildcmp(char *s1, char *s2)
 		}
 	return (*s2 == '\0');
 	}
-       
 	if (*s2 == '*')
 	{
 	return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
@@ -25,5 +24,5 @@ int wildcmp(char *s1, char *s2)
 	{
 	return (wildcmp(s1 + 1, s2 + 1));
 	}
-        return (0);
-}	
+	return (0);
+}
