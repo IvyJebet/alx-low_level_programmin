9e38c327bd9f8d04cae0d4f72a96f2e497848fe9
IvyJebet <jebetivy388@gmail.com>
2023-09-01 19:50:43 -0700

    It ain't what they call you, it's what you answer to Task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include "main.h"
 /**
  * main - Prints the name of the program
+ *
  * @argc: Number of arguments
  * @argv: Array of arguments
  *
@@ -10,5 +10,6 @@
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
+
 	return (0);
 }
