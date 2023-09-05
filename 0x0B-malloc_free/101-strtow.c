5e4d7f1c7e210e214d005eb77719e8758a8b6574
IvyJebet <jebetivy388@gmail.com>
2023-09-05 04:25:36 -0700

    I will show you how great I am Task

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,18 +7,23 @@
  * strtow - Function that splits a string into words
  * @str: String input
  *
- * Return: A pointer to an array of strings
+ * Return: Always 0 (Success)
  * on error, return NULL
  */
 char **strtow(char *str)
 {
+	char *copy = strdup(str);
+	char *token = strtok(copy, " ");
 	char **word_array;
-	char *token = strtok(str, " ");
 	int num_words = 0;
 	int i = 0;
 	int j;
 
 	if (str == NULL || *str == '\0')
+	{
+		return (NULL);
+	}
+	if (copy == NULL)
 	{
 		return (NULL);
 	}
@@ -30,8 +35,10 @@ char **strtow(char *str)
 	word_array = (char **)malloc((num_words + 1) * sizeof(char *));
 	if (word_array == NULL)
 	{
+		free(copy);
 		return (NULL);
 	}
+	token = strtok(str, " ");
 	while (token)
 	{
 		word_array[i] = strdup(token);
@@ -42,11 +49,13 @@ char **strtow(char *str)
 				free(word_array[j]);
 			}
 			free(word_array);
+			free(copy);
 			return (NULL);
 		}
 		i++;
 		token = strtok(NULL, " ");
 	}
 	word_array[num_words] = NULL;
+	free(copy);
 	return (word_array);
 }
