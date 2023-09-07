e348822990aa3cf180b9d1e3c7081660270cd720
IvyJebet <jebetivy388@gmail.com>
2023-09-07 07:55:02 -0700

    _realloc Task

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,6 +1,5 @@
 #include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -34,5 +33,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	min_size = (old_size < new_size) ? old_size : new_size;
 	memcpy(new_ptr, ptr, min_size);
 	free(ptr);
+
 	return (new_ptr);
 }
