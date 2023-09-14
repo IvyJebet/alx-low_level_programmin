e11e46e10b814bdbae20761455b334e841f1b101
IvyJebet <jebetivy388@gmail.com>
2023-09-14 10:23:48 -0700

    variadic_functions.h Fille

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,9 @@
-#ifndef VARIADIC_H
-#define VARIADIC_H
+#ifndef VARIADIC_H_
+#define VARIADIC_H_
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int _putchar(char c)
+int _putchar(char c);
 
 #endif
