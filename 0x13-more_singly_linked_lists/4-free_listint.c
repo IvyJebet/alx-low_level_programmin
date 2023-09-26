855a98d833d630999e675308a00204180ca85dd0
IvyJebet <jebetivy388@gmail.com>
2023-09-25 19:01:42 -0700

    Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - Frees a linked list
+ * @head: Pointer to the head of linked list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head =head->next;
+		free(current);
+	}
+}
